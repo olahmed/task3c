f677076bd1c92563114ece224e2f0168e737ce51
olahmed <149155192+olahmed@users.noreply.github.com>
2024-01-30 19:21:46 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,55 @@
+/**
+ ******************************************************************************
+ * @file           : task3
+ * @author         : ola ahmed
+ * @brief          : Contains the functionality of my application
+ ******************************************************************************
+ */
+
+/* ********************** Includes Section Start ********************** */
+    #include <stdio.h>
+    #include <stdlib.h>
+/* ********************** Includes Section End   ********************** */
+
+/* ********************** Macro Section Start ************************* */
+
+/* ********************** Macro Section End   ************************* */
+
+/* ******************* Global Variables Section Start ************** */
+
+/* ********************** Global Variables  Section End   ************* */
+
+/* ********************** Global Decleration Section Start ************* */
+
+/* ********************** Global Decleration  Section End   ************ */
+
+/* ********************** Sub-Program Declerations Section Start ******* */
+
+/* ********************** Sub-Program Declerations Section End ******* */
+
+/* ********************** Sub-Program Section Start ************* */
+   int main()
+{
+   unsigned int num1;
+   unsigned int num2;
+   unsigned char op;
+   printf("enter the first number : ");
+   scanf("%d",&num1);
+   printf("enter the second number : ");
+   scanf("%d",&num2);
+   printf("enter the operation : ");
+   scanf("%c",&op);
+  /* switch(op)
+   {
+       case('')
+   }*/
+    return 0;
+}
+/* ********************** Sub-Program Section End ************* */
+
+/**
+ ******************************************************************************
+ User          Date                 Brief
+ ******************************************************************************
+ ola ahmed      30jan2024           third task c
+*/
